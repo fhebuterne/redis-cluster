4394f3c01caef45cee72f7816d6bec57d0cd3b58
Fabien <contact@fabien-hebuterne.fr>
2020-01-14 23:49:24 +0100

    fix : use host method now work perfectly

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'redis-cluster' ]; then
     sleep 10
-    echo "yes" | redis-cli --cluster create 173.17.0.2:7001 173.17.0.3:7002 173.17.0.4:7003 --cluster-replicas 0
+    echo "yes" | redis-cli --cluster create 10.0.75.2:7001 10.0.75.2:7002 10.0.75.2:7003 --cluster-replicas 0 --verbose
     echo "DONE"
 else
   exec "$@"
